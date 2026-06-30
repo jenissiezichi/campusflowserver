5857f3197a34ea197a36863091a3bc394ae323f4
Eziokwubundu Jenissi Ezichi <eziokwubundujenissi@gmail.com>
2026-06-30 14:01:58 +0100

    fix idl path for vercel serverless

--- Server/src/configs/solana.js
+++ Server/src/configs/solana.js
@@ -3,6 +3,11 @@ import { Connection, Keypair, PublicKey } from "@solana/web3.js";
 import { readFileSync } from "fs";
 import dotenv from "dotenv";
 
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const connection = new Connection(process.env.SOLANA_NETWORK, "confirmed");
@@ -11,7 +16,7 @@ const secretKey = JSON.parse(process.env.WALLET_SECRET_KEY);
 const keypair = Keypair.fromSecretKey(Uint8Array.from(secretKey));
 const wallet = new anchor.Wallet(keypair);
 
-const idl = JSON.parse(readFileSync("./idl/idl.json", "utf8"));
+const idl = JSON.parse(readFileSync(path.join(__dirname, "../../idl/idl.json"), "utf8"));
 const programId = new PublicKey(process.env.PROGRAM_ID);
 
 const provider = new anchor.AnchorProvider(connection, wallet, {

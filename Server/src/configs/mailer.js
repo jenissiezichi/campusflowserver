f0f3e267d28619cea8dd60b3a892f78157d0ba2b
Prayskey <prayskeyo@gmail.com>
2026-07-02 23:16:24 +0100

    Update code in codebase

--- Server/src/configs/mailer.js
+++ Server/src/configs/mailer.js
@@ -3,19 +3,18 @@ import { configDotenv } from 'dotenv';
 
 configDotenv();
 
-const isDev = process.env.NODE_ENV === 'development';
-
 export const transporter = nodemailer.createTransport({
-  host: '://brevo.com',
+  service: 'gmail',
   port: 587,
   secure: false,
   auth: {
-    // user: process.env.GMAIL_USER,
-    user: "b08066001@smtp-brevo.com",
-    pass: "UZ0JcjmF9fVI1zRk"
-    // pass: process.env.GMAIL_APP_PASS
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
   tls: {
-    rejectUnauthorized: false 
+    rejectUnauthorized: false
   }
 });
+
+// SMTP_USER=prayskeyo@gmail.com
+// SMTP_PASS=UZ0JcjmF9fVI1zRk
--- Server/src/models/admin.model.js
+++ Server/src/models/admin.model.js
@@ -1,8 +1,8 @@
 import pool from "../configs/db.js";
 
 class Admin {
-  static async getAllUsers() {
-    const res = await pool.query("SELECT * FROM users WHERE role IN ('student', 'staff')");
+  static async getAllUsers(universityId) {
+    const res = await pool.query("SELECT * FROM users WHERE role = 'student' AND university_id = $1", [universityId]);
     return res.rows;
   }
 }
